83710264320f1fdf94c066456fce8113ddaf7060
JhonaSan2K <JhonaDev@proton.me>
2026-02-10 17:08:46 +0100

    Cargar autenticación primero

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+require __DIR__.'/auth.php';
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'/*, 'verified'*/])->name('dashboard');
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -11,14 +17,9 @@ Route::resource('games', \App\Http\Controllers\GameController::class);
 Route::resource('ratings', \App\Http\Controllers\RatingController::class);
 Route::resource('comments', \App\Http\Controllers\CommentController::class);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
