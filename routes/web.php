8ff0d78fea0fa2c458d9b824ec874ee123e84f38
JhonaSan2K <JhonaDev@proton.me>
2026-02-13 18:55:22 +0100

    Ajustar ruta

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,31 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\GameController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RatingController;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/auth.php';
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'/*, 'verified'*/])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('games', \App\Http\Controllers\GameController::class);
-Route::resource('ratings', \App\Http\Controllers\RatingController::class);
-Route::resource('comments', \App\Http\Controllers\CommentController::class);
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Custom routes
+
+Route::resource('games', GameController::class);
+Route::resource('ratings', RatingController::class);
+Route::resource('comments', CommentController::class);
+
+
